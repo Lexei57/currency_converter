3ba464010762edaae25db1411ca4a399b1bffcec
Lexei57 <mnzllexei@gmail.com>
2023-03-07 00:10:13 +0200

    Init project and added header branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import {HttpClient} from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'currency-converter';
+  usdRate!: number;
+  eurRate!: number;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getExchangeRates();
+  }
+
+  getExchangeRates() {
+    const apiUrl = 'https://openexchangerates.org/api/latest.json?app_id=18392e1059f8498594bec31628fd3e31&symbols=USD,EUR';
+
+    this.http.get(apiUrl).subscribe((data: any) => {
+      this.usdRate = data.rates.USD;
+      this.eurRate = data.rates.EUR;
+    });
+  }
 }
